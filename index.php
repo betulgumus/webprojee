334539dcc691c512c2825d1d57edf2a17a07aff0
betulgumus <153992737+betulgumus@users.noreply.github.com>
2026-05-03 19:45:38 +0300

    Degisiklik

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container">
-            <a class="navbar-brand" href="index.php">WebProje</a>
+            <a class="navbar-brand" href="index.php">WebProjee</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
